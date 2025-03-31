8c2d5102c081b3811243f46a8526ede7d71cd74e
Ayush <kumarayush68192@gmail.com>
2025-03-31 17:35:44 +0000

    change commit

---
+++ 01.basic/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treating all js code as new version//
+// alert(3+3)// we are using nodejs not browser
+
+
+////number 
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standlone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof"Ayush");
+//type of null is object
---
+++ 01.basic/variables.js
@@ -0,0 +1,16 @@
+const accountId=1444543
+let accountEmail="ayush@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+ 
+let accountState;
+//accountId=12345  nopt valid//
+// prefer not to use var because of issue of block or scope and fuctional scope
+
+accountEmail="ak@google.com"
+accountPassword="123456"
+accountCity="Ahmedabad"
+  
+console.log(accountId)
+
+console.table([accountId, accountEmail,accountPassword, accountCity ,accountState])
