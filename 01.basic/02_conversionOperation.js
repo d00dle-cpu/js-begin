d91a8f2f4d83d7be4e99a258d4bed1c6d15fb912
Ayush <kumarayush68192@gmail.com>
2025-04-01 10:07:18 +0000

    change commit

---
+++ 01.basic/02_conversionOperation.js
@@ -0,0 +1,48 @@
+let score="33abc"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+let isloggedin=1
+let booleanISlogegdin=Boolean(isloggedin)
+//console.log(booleanISlogegdin);
+
+//1=>true;0=>false
+//""=>false
+//"ayush"=>true         
+
+let number=33
+let numtoString=String(number)
+//console.log(typeof numtoString)
+//console.log(numtoString);
+
+
+
+//********************operations****************
+let value=6
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+ 
+let str1="ayush"
+let str2=" prajapati"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
